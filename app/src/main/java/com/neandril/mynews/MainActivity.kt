9d624cd7642688a0dfd72e3c062f593cc10458cf
Rémy <remy.dalla@gmail.com>
2019-06-16 18:08:00 +0200

    Initial commit

---
+++ app/src/main/java/com/neandril/mynews/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.neandril.mynews
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
